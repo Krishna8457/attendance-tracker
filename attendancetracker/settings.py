92dbd4a90b28920fa16e380cc7ec3fbf85ad50be
Krishna8457 <krishnamalik161@gmail.com>
2025-08-10 19:18:09 +0530

    Update ALLOWED_HOSTS for deployment

--- attendancetracker/settings.py
+++ attendancetracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['your-app-name.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['krishna-attendance-tracker.onrender.com', '127.0.0.1']
 
 
 # Application definition
